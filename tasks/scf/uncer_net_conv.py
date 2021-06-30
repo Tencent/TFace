490cf90a1f042b86d7d03042f26d0a7cf6b1f0c0
hitspring2015 <xujianqinghit@163.com>
2021-06-30 10:51:24 +0800

    Update uncer_net_conv.py

--- tasks/scf/uncer_net_conv.py
+++ tasks/scf/uncer_net_conv.py
@@ -33,32 +33,13 @@ class UncertaintyNet_conv(nn.Module):
                         nn.Linear(self.depth // 4, 1),
                         #nn.Softplus())
                         #nn.Sigmoid(),
-                        )
-
-        """
-        self.fc1 = nn.Linear(convf_dim, convf_dim//2)
-        self.bn1 = nn.BatchNorm1d(convf_dim//2, eps=1e-3)
-        self.act = nn.ReLU(inplace=True)
-        self.fc2 = nn.Linear(convf_dim//2, convf_dim//4)
-        self.bn2 = nn.BatchNorm1d(convf_dim//4, eps=1e-3)
-        self.fc3 = nn.Linear(convf_dim//4, 1)
-        """
+        )
 
     def forward(self, convf):
         #kappa = self._kappa(convf) + 8528 #3991472 + 8528
         convf = self.conv_(convf)
         convf = convf.view(convf.shape[0], -1) 
         log_kappa = self._log_kappa(convf)
-       
-        """
-        x = self.fc1(convf)
-        if self.use_bn: x = self.bn1(x)
-        x = self.act(x)
-        x = self.fc2(x)
-        if self.use_bn: x = self.bn2(x)
-        x = self.act(x)
-        log_kappa = self.fc3(x)
-        """
 
         log_kappa = torch.log(1e-6 + torch.exp(log_kappa))
 
